c7cb3d7149fc697fecb7d3bd1a671f56bc2be52c
Mayur Koli <kolimayurs@gmail.com>
2019-04-18 15:32:02 +0530

    secure

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function fetchData(){
 	return function(dispatch){
 		dispatch({type:'FETCH_API_START'})
-		axios.get('http://www.mocky.io/v2/5c8b92a4360000cd198f81b4')
+		axios.get('https://www.mocky.io/v2/5c8b92a4360000cd198f81b4')
 		.then(results => {
 			dispatch({type:'FETCH_API_SUCCESSFUL', payload: results.data})
 		})
